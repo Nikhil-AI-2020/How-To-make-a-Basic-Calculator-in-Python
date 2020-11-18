7bb250668261cd8b49fd7cca52b36236898d2313
Nikhil-AI-2020 <73864905+Nikhil-AI-2020@users.noreply.github.com>
2020-11-18 16:03:10 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+print("    For Addition")
+x=int(input("x = "))
+y=int(input("y = "))
+z=x+y
+print("Your answer is = ",z)
+
+print("    For Subraction")
+x=int(input("x = "))
+y=int(input("y = "))
+z=x-y
+print("Your answer is = ",z)
+
+print("    For Multiplication")
+x=int(input("x = "))
+y=int(input("y = "))
+z=x*y
+print("Your answer is = ",z)
+
+print("    For Division")
+x=int(input("x = "))
+y=int(input("y = "))
+z=x/y
+print("Your answer is = ",z)
